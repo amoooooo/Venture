c982562c8d2c6b73562818c37d133e042199464b
amo <lunarteclipse@gmail.com>
2024-05-06 16:30:22 +0100

    Initial commit

---
+++ src/main/kotlin/com/pokeskies/$mod_id$/config/ExampleModConfig.kt
@@ -0,0 +1,9 @@
+package com.pokeskies.$mod_id$.config
+
+class $mod_name$Config(
+    var debug: Boolean = false
+) {
+    override fun toString(): String {
+        return "ExampleModConfig(debug=$debug)"
+    }
+}
---
+++ src/main/kotlin/com/pokeskies/$mod_id$/utils/SubCommand.kt
@@ -0,0 +1,8 @@
+package com.pokeskies.$mod_id$.utils
+
+import com.mojang.brigadier.tree.LiteralCommandNode
+import net.minecraft.commands.CommandSourceStack
+
+interface SubCommand {
+    fun build(): LiteralCommandNode<CommandSourceStack>
+}
